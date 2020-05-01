502f6590df6e07a7b7f77ee94dd23f82487c0cf2
Kay Markschies <kayschima@web.de>
2020-05-02 01:01:01 +0200

    first real view

--- resources/views/tile.blade.php
+++ resources/views/tile.blade.php
@@ -1,3 +1,28 @@
 <x-dashboard-tile :position="$position">
-<h1>FootballStandings</h1>
+    <h1>{{ $standings['competition']['name'] }}</h1>
+    <table class="table-fixed border text-xs">
+        <thead>
+        <tr>
+            <th>#</th>
+            <th></th>
+            <th>Team</th>
+            <th>Points</th>
+            <th>Goals</th>
+        </tr>
+        </thead>
+        <tbody>
+
+    @foreach($standings['standings'][0]['table'] as $rank)
+            <tr>
+                <td class="border">{{ $rank['position']}}</td>
+                <td class="border"><img style="height: 20px;" src="{{ $rank['team']['crestUrl'] }}" alt="{{ $rank['team']['name']}}"></td>
+                <td class="border">{{ $rank['team']['name']}}</td>
+                <td class="border">{{ $rank['points']}}</td>
+                <td class="border">{{ $rank['goalsFor'] . ' : ' . $rank['goalsAgainst'] }}</td>
+            </tr>
+    @endforeach
+
+        </tbody>
+    </table>
+
 </x-dashboard-tile>
